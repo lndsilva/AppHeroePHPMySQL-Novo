7cb6259db9f9393874b05ab6a738ce1fe0735947
lndsilva <lndsilva@hotmail.com>
2024-09-10 20:57:39 -0300

    Atualização api

--- app/src/main/java/br/senac/sp/l13/appheroephpmysql/Api.java
+++ app/src/main/java/br/senac/sp/l13/appheroephpmysql/Api.java
@@ -2,7 +2,7 @@ package br.senac.sp.l13.appheroephpmysql;
 
 public class Api {
 
-    private static final String ROOT_URL = "http://10.67.96.85/heroeapi/v1/Api.php?apicall=";
+    private static final String ROOT_URL = "http://10.67.96.95/heroeapi/v1/Api.php?apicall=";
 
     public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
     public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
